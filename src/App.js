058d54324bc9665d4146e6337fb6757f5d57455f
ashashishy <ash.ashish.y@gmail.com>
2020-08-03 20:29:48 +0530

    added shops page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Homepage.component.jsx';
 import {Switch, Route} from 'react-router-dom';
-function CameraPage(){
-  return(
-  <div>
-  <h1>Cameras</h1>
-  </div>
-)}
-
+import ShopPage from './pages/homepage/shop/shop.component.jsx';
 
 function App(){
   return(
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/cameras' component={CameraPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
