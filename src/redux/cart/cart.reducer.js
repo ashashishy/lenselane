4bea683cda14f59b75e8f32d24df9c42b878b793
ashashishy <ash.ashish.y@gmail.com>
2020-08-10 09:14:31 +0530

    cart fix

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,7 +2,7 @@ import cartActionTypes from "./cart.actionTypes";
 const INITIAL_STATE = {
   hidden: true,
 };
-export const cartReducer = (state = INITIAL_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case cartActionTypes.TOGGLE_CART_HIDDEN:
       return {
